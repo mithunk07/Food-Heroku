c5e0c42c094303b80e9ae0734d53d5c3e587170b
Mithun Kumar <mithunkumar9940@gmail.com>
2019-08-27 16:25:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open(r'C:\Users\MITHUNKUMAR S\Desktop\Jupyter\Project\FOOD\Flask\model.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
